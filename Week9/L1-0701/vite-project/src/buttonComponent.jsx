7df57d43c28587ecade617616e88d25008d3e167
DanieleGuicciardi <dany.guicciardi@outlook.com>
2025-01-07 15:55:20 +0100

    setting react folder

---
+++ Week9/L1-0701/vite-project/src/buttonComponent.jsx
@@ -0,0 +1,4 @@
+const ButtonComponent = function(props) {
+  return <button>{props.buttonLabel}</button>
+  }
+export default ButtonComponent
---
+++ Week9/L1-0701/vite-project/src/imageComponent.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+class ImageComponent extends Component {
+render() {
+return <img src={this.props.imgSource} alt={this.props.imgAlt}></img>
+}
+}
+
+export default ImageComponent
